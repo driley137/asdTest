4b78668572716c887212bdbad384813c42cd381c
Dustin Riley <driley137@fullsail.edu>
2014-08-29 13:27:45 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+// Dustin S. Riley   ASD1408  - WEEK4
+
+// Global Variables for Design/Color
+var orangeColor = "E08E41";
+var blueColor = "619DAB";
+var offwhiteColor = "D4D0C5";
+var grayColor = "44484D";
+var ltblueColor = "A1BFC4";
+
+// Setting the background color of the application
+Ti.UI.setBackgroundColor(grayColor);
+
+// Requiring files to initiate loads and have global variables to call functions
+var uiFile = require('ui');
+var workFile = require('work');
+var apiFile = require('api');
